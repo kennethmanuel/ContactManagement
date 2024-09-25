de5eca5d0b7b14e7985102d03b1844f18913e527
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2024-09-25 15:07:43 +0700

    migration for RowVersion (conccurency check)

---
+++ Migrations/20240925073338_AddRowVersionColumn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRowVersionColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Version",
+                table: "contact_managemen",
+                nullable: true
+            );
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}

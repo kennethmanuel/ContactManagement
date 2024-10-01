673cb6abe4a4995358d82d245bac3c3478f99d48
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2024-10-01 16:59:44 +0700

    wip

---
+++ Services/VCardService.cs
@@ -0,0 +1,24 @@
+﻿using FolkerKinzel.VCards;
+using FolkerKinzel.VCards.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManagement;
+
+public class VCardService
+{
+    public static byte[] GenerateVCardBasedOnContact()
+    {
+        VCard vCard = VCardBuilder
+          .Create()
+          .NameViews.Add(familyName: "Sonntag", givenName: "Susi")
+          .Phones.Add("+49-321-1234567",
+                       parameters: p => p.PhoneType = Tel.Cell
+                     )
+          .EMails.Add("susi@contoso.com")
+          .EMails.Add("susi@home.de")
+          .EMails.SetPreferences()
+          .VCard;
+
+        return System.Text.Encoding.UTF8.GetBytes(Vcf.ToString(vCard));
+    }
+}
